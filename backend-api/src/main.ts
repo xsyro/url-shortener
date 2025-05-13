e617274f84c8991e01dcdf6759aadece0102ae33
Igbalajobi Jamiu <kreatifcode@gmail.com>
2025-05-13 17:20:43 +0100

    logger level to backend

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'warn', 'log'],
+  });
   app.enableCors({
-    origin: [process.env.CORS_ORIGIN || 'http://localhost:3000'],
+    origin: [
+      process.env.CORS_ORIGIN || 'http://localhost:3000',
+      'http://localhost:3001',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
